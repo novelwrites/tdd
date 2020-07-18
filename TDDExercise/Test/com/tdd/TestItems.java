7a0e945053e56048a971a01c977595c85991b1c7
Nancy Golden <pngolden@verizon.net>
2020-07-18 12:45:33 -0500

    initial project and wrote first test-pass

--- TDDExercise/Test/com/tdd/TestItems.java
+++ TDDExercise/Test/com/tdd/TestItems.java
@@ -7,7 +7,7 @@ public class TestItems {
     Main main = new Main();
     @Test
     public void test_printHelloWorld() {
-        String expected = "Hello John!";
+        String expected = "Hello World!";
         String actual = main.printHelloWorld(); //remember that main is invoking method from main class
 
         assertEquals(expected, actual);
