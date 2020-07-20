05e5ccbbe1d80829c0e99854bbb52788f6591fb8
Nancy Golden <pngolden@verizon.net>
2020-07-19 22:00:08 -0500

    wrote test_isReceiptPrintingTransaction-failed

--- TDDExercise/src/com/tdd/Receipt.java
+++ TDDExercise/src/com/tdd/Receipt.java
@@ -1,4 +1,21 @@
 package com.tdd;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+
 public class Receipt {
+
+
+    public void printOutTransaction(ArrayList<Items> purchasedItems, BigDecimal totalTax, BigDecimal grandTotal) {
+
+        System.out.println("Item Name  :  Price");
+        System.out.println("Total Sales Tax " + totalTax);
+        System.out.println("Grand Total " + grandTotal);
+
+    }
+
+    public String getReceipt(ArrayList<Items>purchasedItems) {
+
+        return "";
+    }
 }
